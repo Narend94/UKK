59305b8ba4c9dcd8d54e3a80aeabab5a9cc28199
Dimas Narendra Putra <112672783+Narend94@users.noreply.github.com>
2023-02-24 14:44:02 +0700

    Add files via upload

---
+++ soalukk/masyarakat/proses_login.php
@@ -0,0 +1,22 @@
+<?php
+    include("koneksi.php");
+
+
+    $username = $_REQUEST["username"];
+    $password = $_REQUEST["password"];
+
+
+
+    $query = $koneksi->query("select * from masyarakat where username='$username' AND password='$password'")->fetchAll();
+    
+    if ($query){
+        $_SESSION["username"] = $_REQUEST['username'];
+        $_SESSION["password"] = $_REQUEST['password'];
+        header("Location:catatan.php");
+    }
+    else{
+        header("Location:index.php");
+    }
+?>
+
+
