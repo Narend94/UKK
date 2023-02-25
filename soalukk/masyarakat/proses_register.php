ccd6d3a423d9cddfb59358ebfb8057add9794daf
Dimas Narendra Putra <112672783+Narend94@users.noreply.github.com>
2023-02-25 14:15:57 +0700

    Add files via upload

---
+++ soalukk/masyarakat/proses_register.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nik = $_POST['nik'];
+    $nama = $_POST['nama'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $telp = $_POST['telp'];
+
+    $koneksi = new PDO("mysql:host=localhost;dbname=pelaporan", 'root', '');
+    $query = $koneksi->query("INSERT INTO masyarakat values('$nik','$nama','$username','$password','$telp')");
+
+    if($query){
+        header("Location:index.php");
+     }
