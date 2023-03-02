3d3113b34213f3189983ff771cc1823fdf8e0d3d
Dimas Narendra Putra <112672783+Narend94@users.noreply.github.com>
2023-03-02 15:33:30 +0700

    Add files via upload

---
+++ soalukk/petugas/proses_register.php
@@ -0,0 +1,14 @@
+<?php
+    $id_petugas = $_POST['id_petugas'];
+    $nama_petugas = $_POST['nama_petugas'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $telp = $_POST['telp'];
+    $level = $_POST['level'];
+
+    $koneksi = new PDO("mysql:host=localhost;dbname=pelaporan", 'root', '');
+    $query = $koneksi->query("INSERT INTO petugas values('$id_petugas','$nama_petugas','$username','$password','$telp','$level')");
+
+    if($query){
+        header("Location:login.php");
+     }
---
+++ soalukk/petugas/proses_tanggapan2.php
@@ -0,0 +1,13 @@
+<?php
+
+    $tanggapan = $_POST['tanggapan'];
+    
+
+    $database = new PDO("mysql:host=localhost;dbname=pelaporan", 'root', '');
+    $query = $database->query("INSERT INTO tanggapan values('$tanggapan')");
+
+    if($query){
+        header("Location:tanggapan.php");
+     }
+
+     
